a7ed938b5f63e8d2f1e57e76dc3dcd1ffc510d21
Majed Alhanash <majed.s.alhanash@gmail.com>
2024-05-01 14:15:01 +0400

    prevent update when connection error

--- app.py
+++ app.py
@@ -91,98 +91,101 @@ app.layout = dbc.Container(
 )
 def update_graph(num, dataset_input):
     """update every 3 seconds"""
-    if dataset_input is None:
+    try:
+        if dataset_input is None:
+            raise PreventUpdate
+        finance = data_handling.get_data(dataset_input)
+        df = finance.data
+        high = finance.day_high
+        low = finance.day_low
+        open = df.Open.iloc[0]
+        last_close = finance.previous_close
+        close = df.Close.iloc[-1]
+
+        if close > last_close:
+            color = {'color':'green'}
+        else:
+            color = {'color':'red'}
+
+        fig = go.Figure(data=go.Ohlc(x=df['Date'],
+                        open=df['Open'],
+                        high=df['High'],
+                        low=df['Low'],
+                        close=df['Close']))
+        
+        fig.update_layout(uirevision=dataset_input,
+                        height=500)
+        
+        fig_close = go.Figure()
+        fig_close.add_trace(go.Indicator(
+            mode = "number+delta",
+            value = last_close,
+            domain = {'row': 0, 'column': 1}))
+        fig_close.update_layout(
+            height=300,
+            template = {'data' : {'indicator': [{
+                'title': {'text': "Last Close"},
+                'mode' : "number",
+                'number' : {'valueformat':'.3f'}},
+                ]
+                                }})
+        
+
+        fig_price = go.Figure()
+        fig_price.add_trace(go.Indicator(
+            mode = "number+delta",
+            value = close,
+            domain = {'row': 0, 'column': 1}))
+        fig_price.update_layout(
+            height=300,
+            template = {'data' : {'indicator': [{
+                'title': {'text': "Price"},
+                'mode' : "number+delta",
+                'number' : {'valueformat':'.3f'},
+                'delta' : {'reference': last_close, 'valueformat':'.3f','suffix': f" ({(close-last_close)/last_close:.3%}) {finance.currency}"}}]
+                                }})
+
+        fig_open = go.Figure()
+        fig_open.add_trace(go.Indicator(
+            mode = "number+delta",
+            value = open,
+            domain = {'row': 0, 'column': 1}))
+        fig_open.update_layout(
+            height=300,
+            template = {'data' : {'indicator': [{
+                'title': {'text': "Open"},
+                'mode' : "number+delta",
+                'number' : {'valueformat':'.3f'},}]
+                                }})
+
+        fig_high = go.Figure()
+        fig_high.add_trace(go.Indicator(
+            mode = "number+delta",
+            value = high,
+            domain = {'row': 0, 'column': 1}))
+        fig_high.update_layout(
+            height=300,
+            template = {'data' : {'indicator': [{
+                'title': {'text': "High"},
+                'mode' : "number+delta",
+                'number' : {'valueformat':'.3f'}}]
+                                }})
+
+
+        fig_low = go.Figure()
+        fig_low.add_trace(go.Indicator(
+            mode = "number+delta",
+            value = low,
+            domain = {'row': 0, 'column': 1}))
+        fig_low.update_layout(
+            height=300,
+            template = {'data' : {'indicator': [{
+                'title': {'text': "Low"},
+                'mode' : "number+delta",
+                'number' : {'valueformat':'.3f'}}]
+                                }})
+    except:
         raise PreventUpdate
-    finance = data_handling.get_data(dataset_input)
-    df = finance.data
-    high = finance.day_high
-    low = finance.day_low
-    open = df.Open.iloc[0]
-    last_close = finance.previous_close
-    close = df.Close.iloc[-1]
-
-    if close > last_close:
-        color = {'color':'green'}
-    else:
-        color = {'color':'red'}
-
-    fig = go.Figure(data=go.Ohlc(x=df['Date'],
-                    open=df['Open'],
-                    high=df['High'],
-                    low=df['Low'],
-                    close=df['Close']))
-    
-    fig.update_layout(uirevision=dataset_input,
-                      height=500)
-    
-    fig_close = go.Figure()
-    fig_close.add_trace(go.Indicator(
-        mode = "number+delta",
-        value = last_close,
-        domain = {'row': 0, 'column': 1}))
-    fig_close.update_layout(
-        height=300,
-        template = {'data' : {'indicator': [{
-            'title': {'text': "Last Close"},
-            'mode' : "number",
-            'number' : {'valueformat':'.3f'}},
-            ]
-                             }})
-    
-
-    fig_price = go.Figure()
-    fig_price.add_trace(go.Indicator(
-        mode = "number+delta",
-        value = close,
-        domain = {'row': 0, 'column': 1}))
-    fig_price.update_layout(
-        height=300,
-        template = {'data' : {'indicator': [{
-            'title': {'text': "Price"},
-            'mode' : "number+delta",
-            'number' : {'valueformat':'.3f'},
-            'delta' : {'reference': last_close, 'valueformat':'.3f','suffix': f" ({(close-last_close)/last_close:.3%})"}}]
-                             }})
-
-    fig_open = go.Figure()
-    fig_open.add_trace(go.Indicator(
-        mode = "number+delta",
-        value = open,
-        domain = {'row': 0, 'column': 1}))
-    fig_open.update_layout(
-        height=300,
-        template = {'data' : {'indicator': [{
-            'title': {'text': "Open"},
-            'mode' : "number+delta",
-            'number' : {'valueformat':'.3f'},}]
-                             }})
-
-    fig_high = go.Figure()
-    fig_high.add_trace(go.Indicator(
-        mode = "number+delta",
-        value = high,
-        domain = {'row': 0, 'column': 1}))
-    fig_high.update_layout(
-        height=300,
-        template = {'data' : {'indicator': [{
-            'title': {'text': "High"},
-            'mode' : "number+delta",
-            'number' : {'valueformat':'.3f'}}]
-                             }})
-
-
-    fig_low = go.Figure()
-    fig_low.add_trace(go.Indicator(
-        mode = "number+delta",
-        value = low,
-        domain = {'row': 0, 'column': 1}))
-    fig_low.update_layout(
-        height=300,
-        template = {'data' : {'indicator': [{
-            'title': {'text': "Low"},
-            'mode' : "number+delta",
-            'number' : {'valueformat':'.3f'}}]
-                             }})
     
     return(finance.symbol, color,fig, fig_price, fig_open, fig_high, fig_low, fig_close)
 
